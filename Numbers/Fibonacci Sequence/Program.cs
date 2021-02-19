ca0db1931b945b6e8b9271acec4688c56b205d66
Aristotle Maule <aristotle.maule@hotmail.com>
2021-02-19 17:30:13 -0500

    Generated Fibonacci

---
+++ Numbers/Fibonacci Sequence/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+/*Fibonacci Sequence - 
+ * Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number.*/
+
+namespace Fibonacci_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many numbers of the Fibonacci Sequence?");
+            string input = Console.ReadLine();
+            int count = Int32.Parse(input);
+
+            if (count >= 20 || count < 0)
+            {
+                Console.WriteLine("Sorry that number was to large");
+            }
+            else
+            {
+                for (var i = 0; i < count; i++)
+                {
+                    Console.WriteLine(fibCalc(i));
+                }
+            }
+            
+        }
+
+        static int fibCalc(int counter)
+        {
+            if(counter == 0)
+                return 0;
+            if(counter == 1)
+                return 1;
+
+
+            int firstNumber = 0;
+            int secondNumber = 1;
+            int result = 0;
+
+            for (var i = 2; i <= counter; i++)
+            {
+                result = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = result;
+
+            }
+            return result;
+        }
+    }
+}
