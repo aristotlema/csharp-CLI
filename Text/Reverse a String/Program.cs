3373627fddee5140e03ad8e19b45ab6d6803b8e5
Aristotle Maule <aristotle.maule@hotmail.com>
2021-02-20 10:54:09 -0500

    Reverse a string

---
+++ Text/Reverse a String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*    Reverse a String 
+ *    Enter a string and the program will reverse it and print it out.*/
+
+
+namespace Reverse_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string, I will reverse it");
+            string inputString = Console.ReadLine();
+            string outputString = "";
+
+
+            for(var i = inputString.Length; i --> 0;)
+            {
+                outputString += inputString[i];
+                
+            }
+
+            Console.WriteLine(outputString);
+
+        }
+    }
+}
