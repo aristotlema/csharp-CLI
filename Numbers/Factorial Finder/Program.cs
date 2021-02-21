ba6dcc1d807daf7d8ce3d258f02f41f48e8e6a2e
Aristotle Maule <aristotle.maule@hotmail.com>
2021-02-21 14:54:50 -0500

    Factorials completed

---
+++ Numbers/Factorial Finder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/*Factorial Finder 
+ * The Factorial of a positive integer, n, is defined as the product of the sequence n, n-1, n-2, ...1 and the factorial of zero, 0, is defined as being 1. Solve this using both loops and recursion.*/
+
+namespace Factorial_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FactorialForLoop(7));
+
+            Console.WriteLine(FactorialRecursion(5));
+        }
+
+        static int FactorialForLoop(int input)
+        {
+            int currentTotal = input * (input - 1);
+            for(var i = input - 2; i > 0; i--)
+            {
+                currentTotal = i * currentTotal;
+            }
+            return currentTotal;
+        }
+        static int FactorialRecursion(int input)
+        {
+            if (input == 1) 
+                return 1;
+            return input * FactorialRecursion(input - 1);
+        }
+    }
+}
