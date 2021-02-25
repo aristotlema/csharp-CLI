809b7f524f5a568d8f092f076a3926e072bc58ab
Aristotle Maule <aristotle.maule@hotmail.com>
2021-02-25 07:55:18 -0500

    Basic EF setup

---
+++ Tutorials/Entity Framework/Entity FrameWork/Migrations/20210225125247_Fix.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entity_FrameWork.Migrations
+{
+    public partial class Fix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
